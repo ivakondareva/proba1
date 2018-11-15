15718c870df9576c6ac84555d8e6b57817aff4d0
danisini <dani.levski00@gmail.com>
2018-11-15 10:10:29 +0200

    Add files via upload
    Simple 3 hyperlinks

---
+++ Home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html 5.0>
+<html>
+<head>
+<meta http-equiv="Content-type" content="text/html' charset=utf8">
+<title> data </title>
+</head>
+<style>
+li{
+    float:left;
+}
+li a {
+    display:block;
+    padding: 14px 16px;
+}
+</style>
+<body>
+<ul>
+
+    <li><a href="proba1.php">PersonList</a></li>
+    <li><a href="proba2.php">EditPerson</a></li>
+    <li><a href="about">About</a></li>
+</ul>
+</body>
+</html>
