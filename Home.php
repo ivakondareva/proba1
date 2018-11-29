dffa1a0cba21108bdbf1d26509dc18aec54ba156
Iva <hannah333@abv.bg>
2018-11-29 11:36:59 +0200

    Update Home.php

--- Home.php
+++ Home.php
@@ -24,38 +24,9 @@
 <link rel="stylesheet" type="text/css" href="Styling/main/css/pages/dashboard1.css">
 <link rel="stylesheet" type="text/css" href="Styling/main/css/pages/dashboard2.css">
 -->
-
+<link rel="import" href="nav.html">
 <style>
-body {
-    background-color:white;
-}
-h1{
-    padding: 60px;
-  text-align: center;
-  background: #a585d3;
-  color: black;
-  font-size: 50px;
-}
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #a585d3;
-}
-li{
-    float:left;
-}
-li a {
-    display: block;
-    color: black;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
-li a:hover {
-    background-color: #52377a;
-}
+
 * {box-sizing: border-box}
 /* Add padding to containers */
 .container {
@@ -100,13 +71,9 @@ hr {
 }
 </style>
 <body>
-<h1> Project for Dorothy </h1>
-<ul>
+<div w3-include-html="nav.html"></div> 
+</br>
 
-    <li><a href="proba1.php">PersonList</a></li>
-    <li><a href="proba2.php">EditPerson</a></li>
-    <li><a href="about">About</a></li>
-</ul>
 <form action="action_page.php">
   <div class="container">
     <h2>Log In</h2>
