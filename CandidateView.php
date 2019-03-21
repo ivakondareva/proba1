b9bf34223e6750a94b49a1be558fdaaa322e07ad
danisini <dani.levski00@gmail.com>
2019-03-21 21:55:00 +0200

    Renamed

---
+++ CandidateView.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html 5.0>
+<html>
+<head>
+<link rel="import" href="nav.php">
+<meta http-equiv="Content-type" content="text/html' charset=utf8">
+
+<title> data </title>
+</head>
+<body>
+<div w3-include-html="nav.php"></div> 
+<style>
+input.dotted {border-style: dotted;}
+input.dashed {border-style: dashed;}
+input.solid {border-style: solid;}
+input.double {border-style: double;}
+input.groove {border-style: groove;}
+input.ridge {border-style: ridge;}
+input.inset {border-style: inset;}
+input.outset {border-style: outset;}
+input.none {border-style: none;}
+input.hidden {border-style: hidden;}
+input.mix {border-style: dotted dashed solid double;}
+</style>
+</br>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$conn = mysqli_connect($servername, $username, $password);
+$database= mysqli_select_db($conn, 'dorothy');
+$idFound=-1;
+$Id=0;
+$prom;
+if(isset($_REQUEST["id"]))
+{
+    $GLOBALS['Id'] = urldecode($_REQUEST["id"]);
+    $GLOBALS['prom']=$GLOBALS['Id'];
+    $word=array(
+        "Id",
+        "Name",
+        "Email",
+        "PhoneNumber",
+        "Sex",
+        "DateOfBirth",
+        "CV",
+        "Resume",
+        "Position",
+        "AddedBy",
+        "DateOfApplication",
+        "IsCandidate");
+    $i=0;
+    $arr=array();
+    for($i=0;$i<count($word);$i++)
+    {
+        $NqkvoId = $GLOBALS['Id'];
+        $sql = "SELECT $word[$i] FROM candidates Where Id=$NqkvoId";
+        if (mysqli_multi_query($conn,$sql))
+        {
+            do
+            {
+                if ($sql=mysqli_store_result($conn))
+                {
+                    while ($row=mysqli_fetch_row($sql))
+                    {    
+                       $arr[$i]=$row[0];
+                    }
+                    mysqli_free_result($sql);
+                }
+            }while (mysqli_more_results($conn) && mysqli_next_result($conn));
+        }
+    }
+    "</br>";
+    /*    $tempId=$arr[0];
+        echo "<table>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone Number</th>";
+            echo "<tr>";
+            for($i=1;$i<10;$i++)
+            {
+                echo "<td>";
+                echo "<center>";
+                $tempFeat=$arr[$i];
+                if($i==1)
+                {echo "<a href='Candidate.php?id=$tempId' style='color: RGB(103,117,124)'> $tempFeat
+                </a>";}
+                else printf("%s</br>",$tempFeat);
+                echo "</td>";
+            }
+            echo "</tr>";
+        echo "</table>";
+    */    
+    $GLOBALS['Id'] = $arr[0];
+    $Name = $arr[1];
+    $Email = $arr[2];
+    $PhoneNumber = $arr[3];
+    if($arr[4]==1) $Sex="Мъж";
+    else $Sex="Жена";
+    $DateOfBirth = $arr[5];
+    $CV = $arr[6];
+    $Resume = $arr[7];
+    $Position = $arr[8];
+    $AddedBy = $arr[9];
+    if(isset($arr[10])==1)
+    $DateOfApplication = $arr[10];
+    else
+    $DateOfApplication = "No information";
+    $IsCandidate=$arr[11];
+    $images = array("$Name".".jpg");
+    $User= urldecode($_REQUEST["user"]);
+    // Loop through array to create image gallery
+
+    foreach($images as $image)
+    {
+        echo '<center>
+            <form action="Candidate.php?user='.$User.'&isCandidate='.$IsCandidate.'" method="post" enctype="multipart/form-data">
+            <input type="text" name = "InputId" value='.$GLOBALS["Id"].' hidden/>
+            <input type="text" name = "NameZaDolu" value="'.$GLOBALS["Name"].'" hidden/>
+            <input type="text" name = "EmailZaDolu" value="'.$GLOBALS["Name"].'" hidden/>
+            <input type="text" name = "PNZaDolu" value="'.$GLOBALS["Name"].'" hidden/>
+            <input type="text" name = "SexZaDolu" value="'.$GLOBALS["Name"].'" hidden/>
+            <input type="text" name = "DoBZaDolu" value="'.$GLOBALS["Name"].'" hidden/>
+            <input type="text" name = "ResumeZaDolu" value="'.$GLOBALS["Name"].'" hidden/>
+            <input type="text" name = "PosZaDolu" value="'.$GLOBALS["Name"].'" hidden/>
+            <div class="img-box">
+                <img src="Files/' . $image . '" width="150" alt="' .  pathinfo($image, PATHINFO_FILENAME) .'">
+            </div>
+            </br>
+            <label>NAME:</label>
+            <input type="text"  class="hidden" value="'.$GLOBALS["Name"].'" name="InputName"/>
+            </br>
+            <label>Email:</label>
+            <input type="text"  class="hidden" value="'.$GLOBALS["Email"].'" name="InputEmail"/>
+            </br>
+            <label>Phone Number:</label>
+            <input type="text"  class="hidden" value="'.$GLOBALS["PhoneNumber"].'" name="InputPhoneNumber"/>
+            </br>
+            <label>Sex:</label>
+            <input type="text"  class="hidden" value="'.$GLOBALS["Sex"].'" name="InputSex"/>
+            </br>
+            <label>Date Of Birth:</label>
+            <input type="text"  class="hidden" value="'.$GLOBALS["DateOfBirth"].'" name="InputDoB"/>
+            </br>
+            <label>Resume:</label>
+            <input type="text"  class="hidden" value="'.$GLOBALS["Resume"].'" name="InputResume"/>
+            </br>
+            <label>Position:</label>
+            <input type="text"  class="hidden" value="'.$GLOBALS["Position"].'" name="InputPos"/>
+            </br>
+            <label>AddedBy:</label>
+            <label>'.$AddedBy.'</label>
+            </br>
+            <label>Date Of Application:</label>
+            <label>'.$DateOfApplication.'</label>
+            </br>
+            <input type="radio" name="TurnIt" value="1"/>Employee
+            <input type="radio" name="TurnIt" checked="checked" value="0"/>Candidate
+            </br>
+            </select> 
+            </br>
+            <input type="submit" name="Save" value="SaveMe" />
+            </form>
+            </center>';
+    }
+}
+    
+function Change()
+{
+    $ChangeName = $_POST['InputName'];
+    $ChangeId=$_POST['InputId'];
+    $ChangeEmail = $_POST['InputEmail'];
+    $ChangePN = $_POST['InputPhoneNumber'];
+    if($_POST['InputSex']=="Мъж")
+        $ChangeSex = 1;
+    else if ($_POST['InputSex']=="Жена")
+        $ChangeSex = 0;
+    else 
+        echo "Не може джендъри, има само два пола!" . $GLOBALS['conn']->error;
+    $ChangeDoB = $_POST['InputDoB'];
+    $ChangeResume = $_POST['InputResume'];
+    $ChangePos = $_POST['InputPos'];
+    if(isset($_POST['TurnIt']))
+    {
+        $ChangePosType=1-$_POST['TurnIt'];
+        $sql = "UPDATE Candidates SET Name='$ChangeName', Email='$ChangeEmail', PhoneNumber='$ChangePN', Sex='$ChangeSex', DateOfBirth='$ChangeDoB', Resume='$ChangeResume', Position='$ChangePos', IsCandidate='$ChangePosType' WHERE id=$ChangeId";
+        echo "good camila";
+        if ($GLOBALS['conn']->query($sql) === FALSE) 
+            echo "Error updating record: " . $GLOBALS['conn']->error;
+    }
+    else
+    {
+        $sql = "UPDATE Candidates SET Name='$ChangeName', Email='$ChangeEmail', PhoneNumber='$ChangePN', Sex='$ChangeSex', DateOfBirth='$ChangeDoB', Resume='$ChangeResume', Position='$ChangePos' WHERE id=$ChangeId";
+        echo "good camila";
+        if ($GLOBALS['conn']->query($sql) === FALSE) 
+            echo "Error updating record: " . $GLOBALS['conn']->error;
+    }
+    $GLOBALS['Name']=$_POST['NameZaDolu'];
+}
+if(isset($_POST['Save']))
+{
+    Change();
+    $ChangeName = $_POST['InputName'];
+    $NameZaSnimka = $_POST['NameZaDolu'];
+    rename("Files/".$NameZaSnimka.'.jpg', "Files/".$ChangeName.".jpg");
+    header($userche);
+}
+mysqli_close($conn);
+?>
+</body>
+</html>
---
+++ PersonList.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html 5.0>
+<head>
+<!--
+<style>
+table {
+    width:70%; 
+    margin-left:20%; 
+    margin-right:20%;
+  }
+table, th, td {
+    border-bottom: 1px solid black;
+	width: 60%;
+}
+</style>
+-->
+<style>
+p {
+  text-align:center;
+  font-size: 30px;
+  font-weight: bold;
+}
+th {
+	padding: 20px;
+	margin:20px;
+}
+</style>
+<link rel="import" href="nav.php">
+
+<style>
+.btn-group .button {
+  background-color: black; /* Green */
+  border: 1px solid white;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  cursor: pointer;
+  width: 200px;
+  display: block;
+}
+.btn-group .button:not(:last-child) {
+  border-bottom: none; /* Prevent double borders */
+}
+.btn-group .button:hover {
+  background-color: #708090;
+}
+.button {
+	float:left;
+  display: inline-block;
+  padding: 15px 25px;
+  font-size: 24px;
+  cursor: pointer;
+  text-align: center;
+  text-decoration: none;
+  outline: none;
+  color: #fff;
+  background-color: black;
+  border: none;
+  border-radius: 15px;
+  box-shadow: 0 9px #999;
+}
+.button:active {
+  background-color: black;
+  box-shadow: 0 5px white;
+  transform: translateY(4px);
+}
+</style>
+
+<meta http-equiv="Content-type" content="text/html' charset=utf8">
+<title> data </title>
+</head>
+<body>
+<div w3-include-html="nav.php"></div> 
+</br>
+<?php
+session_start();
+if(isset($_POST['candidatesBtn']))
+{
+    $userche='location:proba1.php?user='.$_SESSION['username'].'&isCandidate=1';
+    header($userche);
+}
+else if(isset($_POST['employeesBtn']))
+{
+      $userche='location:proba1.php?user='.$_SESSION['username'].'&isCandidate=0';
+      header($userche);
+}
+?>
+<?php
+if (isset($_SESSION['username']) && isset($_SESSION['loggedin']) && $_SESSION['loggedin']==true && $_SESSION['username']==$_REQUEST["user"])
+{
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    
+    $conn = mysqli_connect($servername, $username, $password);
+    $database= mysqli_select_db($conn, 'dorothy');
+    $word=array(
+        "Id",
+        "Name",
+        "Email",
+        "PhoneNumber",
+        "IsCandidate");
+    $i=0;
+    $arr=array();
+    for($i=0;$i<5;$i++)
+    {
+        $sql = "SELECT $word[$i] FROM candidates ORDER BY Name";
+        $ind=0;
+        if (mysqli_multi_query($conn,$sql))
+        {
+            do
+            {
+                if ($sql=mysqli_store_result($conn))
+                {
+                    while ($row=mysqli_fetch_row($sql))
+                    {
+                        $arr[$i][$ind]=$row[0];
+                        $ind++;
+                    }
+                    mysqli_free_result($sql);
+                }
+            }while (mysqli_more_results($conn) && mysqli_next_result($conn));
+        }
+    }
+    $j=0;
+	  "</br>";
+    echo "<form action='proba1.php' method='post'  class='btn-group' enctype='multipart/form-data'>
+	       <button type='submit' name='candidatesBtn'  class='button' > See Candidates </button>
+	       <button type='submit' name='employeesBtn' class='button' > See Employees </button>
+	       </form>";
+	  echo "<br>";
+	  echo "<br>";
+	  echo "<br>";
+    echo "<table>
+    <th>Име</th>
+	  <th>E-mail
+	  </th>
+    <th>Телефонен Номер</th>";
+    $isCand=$_REQUEST["isCandidate"];
+    for($j=0;$j<$ind;$j++)
+    {
+        if($arr[4][$j]==$isCand)
+        {
+          	echo "<tr>";
+          	for($i=1;$i<4;$i++)
+          	{   
+                echo "<td>";
+                echo "<center>";
+                $temp=$arr[0][$j];
+                $tempname=$arr[$i][$j];
+                if($i==1)
+                {
+                    if($isCand==1)
+                	  {
+                		    $user=$_SESSION['username'];
+                		    echo "<a href='Candidate.php?user=$user&id=$temp' style='color: RGB(103,117,124)'> $tempname
+                		    </a>";
+                		    echo "</td>";
+                	  }
+                	  else
+                	  {
+                		    $user=$_SESSION['username'];
+                		    echo "<a href='Employee.php?user=$user&id=$temp' style='color: RGB(103,117,124)'> $tempname
+                		    </a>";
+                		    echo "</td>";
+                	  }
+                }
+                else 
+                {
+                  	printf("%s</br>",$arr[$i][$j]);
+                  	echo "</td>";
+                }
+            }
+          	echo "</tr>";
+      	}
+    }
+    echo "</table>";
+    mysqli_close($conn);
+}
+else
+{s
+  	echo "<p>";
+  	$bam="За да видите тази страница, моля, влезте в профила си!";
+  	printf("%s",$bam);
+  	echo "<br>";
+  	echo "<a href='Home.php'>Вход</a>";
+  	echo "</p>";
+}
+?>
+</body>
+</html>
